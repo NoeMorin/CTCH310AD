ca7039ef3ede7f421a2dd6400615466a2d8243ea
NoeMorin <33008276+NoeMorin@users.noreply.github.com>
2019-02-05 11:13:20 -0600

    Camera traversal and movement
    Coded axis movement in realtion to looking with a mouse and added forward, back, left, and right movement to the arrow keys.

--- New Unity Project/Assets/NewBehaviourScript.cs
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -7,18 +7,60 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //print("hello world");   
+          
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        move();
+    }
+
+    void move()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.z++;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.z--;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             Vector3 position = this.transform.position;
             position.x--;
             this.transform.position = position;
         }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+        float z = transform.eulerAngles.z;
+
+        transform.Rotate(0, 0, -z);
+
+        transform.Rotate(-v, h, 0);
+        //also
+        //transform.Rotate(-v,0,0);
+        //transform.Rotate(0,h,0);
+        //if(this.transform.rotation.eulerAngles.z!= 0)
+        //{
+        //this.tranform.Rotate(0,0, -This.transform.eulerAngles.z);
+        //}
+
 
     }
 }
+//fix Z axis; only allow range of movement along X axis
+//camera.look where to move(fix arrowkey problem)
+//deliver C# code assignment 2
