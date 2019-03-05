6a837d97d92803e267a12902584820c76233d715
NoeMorin <33008276+NoeMorin@users.noreply.github.com>
2019-03-05 11:01:38 -0600

    Adding in controls for a switch statement

--- New Unity Project/Assets/NPCprototyping.cs
+++ New Unity Project/Assets/NPCprototyping.cs
@@ -7,6 +7,7 @@ public class NPCprototyping : MonoBehaviour
 
     public int intel = 5;
     public bool hitonhead = true;
+    public bool readbook = true;
 
     float test()
     {
@@ -29,25 +30,65 @@ public class NPCprototyping : MonoBehaviour
 
             case 4:
                 print("Ello guv!");
+                if (hitonhead)
+                {
+                    intel = 3;
+                    Greet();
+                }
+                if (readbook)
+                {
+                    intel = 5;
+                    Greet();
+                }
                 break;
 
             case 3:
                 print("What you want?");
+                if (hitonhead)
+                {
+                    intel = 2;
+                    Greet();
+                }
+                if (readbook)
+                {
+                    intel = 5;
+                    Greet();
+                }
                 break;
 
             case 2:
                 print("Ugh... Ugh... Hungry...");
+                if (hitonhead)
+                {
+                    intel = 1;
+                    Greet();
+                }
+                if (readbook)
+                {
+                    intel = 5;
+                    Greet();
+                }
                 break;
 
             case 1:
                 print("Grrrrrr...");
+                if (hitonhead)
+                {
+                    intel = default;
+                    Greet();
+                }
+                if (readbook)
+                {
+                    intel = 5;
+                    Greet();
+                }
                 break;
 
             default:
                 print("...");
                 break;
         }
-
+        //add in hit on head to make dumber with spacebar; pressing r key will make the ogre read a book and get smarter
     }
 
     // Start is called before the first frame update
