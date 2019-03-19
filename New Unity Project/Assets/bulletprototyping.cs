eab9eaf0d4adbe3a39cbf94300da975ef189d134
NoeMorin <33008276+NoeMorin@users.noreply.github.com>
2019-03-19 10:59:58 -0600

    Sound effects lab
    Added in sound effects for 'shooting' a projectile

--- New Unity Project/Assets/bulletprototyping.cs
+++ New Unity Project/Assets/bulletprototyping.cs
@@ -6,6 +6,17 @@ public class bulletprototyping : MonoBehaviour
 {
     public GameObject bullet;
     public Transform bulletspawn;
+    public AudioClip pewpew;
+
+    private AudioSource source;
+    private float volLowRange = 0.5f;
+    private float volHighRange = 1.0f;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +28,9 @@ public class bulletprototyping : MonoBehaviour
     {
 
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Verical") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
+        //ties 'gun' to camera
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
 
@@ -29,6 +41,8 @@ public class bulletprototyping : MonoBehaviour
     }
     void Fire()
     {
+
+        Pew();
         //Creating bullet from bullet prefab
         var newBullet = (GameObject)Instantiate(
             bullet,
@@ -40,6 +54,14 @@ public class bulletprototyping : MonoBehaviour
 
         //Destroy the bullet after 2 seconds
         Destroy(newBullet, 2.0f);
+
+    }
+
+    //shot sound
+    void Pew()
+    {
+        float vol = Random.Range(volLowRange, volHighRange);
+        source.PlayOneShot(pewpew, vol);
 
     }
 }
