b2505361fc66b7cc03823d965205be68abb717c1
NoeMorin <33008276+NoeMorin@users.noreply.github.com>
2019-04-02 11:03:54 -0600

    Pause menu Lab
    Constructed a simple pause menu using UI functions in Unity. These same functions can be used for other elements of a UI.

---
+++ New Unity Project/Assets/Manager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField]
+
+    Transform UIPanel;
+    bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIPanel.gameObject.SetActive(false);
+        isPaused = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !isPaused)
+        {
+            Pause();
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && isPaused)
+        {
+            UnPause();
+        }
+
+    }
+    public void Pause()
+    {
+        isPaused = true;
+        UIPanel.gameObject.SetActive(true);
+    }
+    public void UnPause()
+    {
+        isPaused = false;
+        UIPanel.gameObject.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void Restart()
+    {
+        Application.LoadLevel(0);
+    }
+
+}
